2f273a000c4ca3979a58f5209bfad952fb53bd7f
jbrownssf <jpbrown@softstackfactory.org>
2017-04-20 01:21:49 +0000

    first push

---
+++ TKTest/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { LobbyPage } from '../pages/lobby-page/lobby-page';
+import { MyApp } from './app.component';
+import { QuestionPage } from '../pages/question-page/question-page';
+
+let injections: any[] = [MyApp, LobbyPage];
+
+@NgModule({
+  declarations: injections,
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: injections,
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
