592572d42adb8d7770652cd36467284253195b93
jbrownssf <jpbrown@softstackfactory.org>
2017-04-25 02:01:32 +0000

    question page

--- TKTest/src/app/app.component.ts
+++ TKTest/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {LobbyPage } from '../pages/lobby-page/lobby-page';
+import { LobbyPage } from '../pages/lobby-page/lobby-page';
 @Component({
   templateUrl: 'app.html'
 })
